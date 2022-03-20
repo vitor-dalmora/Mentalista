f5e3b7c58f2b6ab49bf489593b44c6966a5aac4b
Vitor <vgabrieldalmora@gmail.com>
2022-03-20 19:18:57 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+    var elementoResultado = document.getElementById("resultado");
+    var chute = parseInt(document.getElementById("valor").value);
+
+    if (chute == numeroSecreto) {
+        elementoResultado.innerHTML = 'Você Acertou!';
+    }
+
+    else if (chute > 10 || chute < 0) {
+        elementoResultado.innerHTML = 'Você deve digitar um número de 0 a 10';
+    }
+
+    else {
+        elementoResultado.innerHTML = 'Você Errou!';
+    }
+
+}
+
